3c33c7263c6c99d762bf32c61b35a7680ffe4478
josefineon <144671788+josefineon@users.noreply.github.com>
2025-03-14 10:16:37 +0100

    Update LMM_script_restricted.R

--- LMM_script_restricted.R
+++ LMM_script_restricted.R
@@ -1,33 +1,5 @@
 
-
-# This script is provided in relation to the data analysis described in Section 2.5 of the Methods section of the article:
-# Title: The effect of the discontinued use of zinc oxide on antimicrobial usage in Danish pig farms
-#
-# The primary objective of the analysis, described in the article,
-# was to quantify the effect of the nationwide ban on zinc oxide usage, 
-# as well as the influence of individual farms and veterinarians, on farm-level AMU.
-#
-# Using pig-farm and prescription data from 2018 to 2023 from Danish national databases, 
-# we fitted a linear mixed-effect model to a three-level nested dataset, consisting of 
-# monthly average standardized AMU (Defined Animal Daily Doses per pig-day), on a farm (n=4,020), overseen by a veterinarian (n=146).
-#
-# The script is shared to facilitate transparency, ensure reproducibility, and make the methodology accessible.
-#
-# The data provided with this script has been anonymized to ensure compliance with the General Data Protection Regulation (GDPR).
-# Specifically, all farm characteristics have been removed, and farm and vet ID numbers have been altered to protect confidentiality.
-#
-# While the results from this version may not match exactly with those in the article, 
-# due to the removal of structural fixed effects from the linear mixed-effects model, 
-# the script remains consistent with the methodology outlined in the article.
-#
 # Generative AI tools have been applied in the development of this script.
-#
-# For any questions, please contact Josefine Ostenfeld Nielsen (josos@dtu.dk).
-
-
-
-
-
 
 
 
